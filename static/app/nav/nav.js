959054461e2775263e6252a957beb00d9a8fc87e
CuongNC <cuongnc@gonrin.com>
2020-02-07 23:01:42 +0700

    update nav

--- static/app/nav/nav.js
+++ static/app/nav/nav.js
@@ -5,47 +5,75 @@ define(function (require) {
 		Gonrin = require('gonrin');
 	return [
 		{
-			"text": "Chi tiết Báo cáo tổng hợp nghi ngờ nhiễm bệnh",
-			"icon": "fa fa-calendar",
-			"type": "view",
-			"collectionName": "baocaotonghopnghingonhiembenh",
-			"route": "baocaotonghopnghingonhiembenh/collection",
-			"$ref": "app/view/BaoCaoTongHopNghiNgoNhiemBenh/ModelView",
-			"visible": function () {
-				return false;
-			}
+			"text": "Index",
+			"type":"view",
+			"collectionName":"index",
+			"route":"index",
+			"$ref": "app/bases/IndexView",
+			"visible": false
 		},
 		{
-			"text": "Báo cáo tổng hợp nghi ngờ nhiễm bệnh",
-			"icon": "fa fa-calendar",
-			"type": "view",
-			"collectionName": "baocaotonghopnghingonhiembenhnhoma",
-			"route": "baocaotonghopnghingonhiembenhnhoma/collection",
-			"$ref": "app/view/BaoCaoTongHopNghiNgoNhiemBenh/ModelView",
-			"visible": function () {
-				return true
-			}
+			"text": "Báo cáo cửa khẩu",
+			"icon":static_url + "/images/icons/task_120.png",
+			"type":"category",
+			"entries":[
+				{
+					"text": "Tổng hợp nghi ngờ nhiễm bệnh",
+					"icon": "fa fa-calendar",
+					"type": "view",
+					"collectionName": "baocaotonghopnghingonhiembenhnhoma",
+					"route": "baocaotonghopnghingonhiembenhnhomack/collection",
+					"$ref": "app/view/BaoCaoTongHopNghiNgoNhiemBenh/ModelView",
+					"visible": function () {
+						return true
+					}
+				},
+				
+				{
+					"text": "Tờ khai y tế đối với người",
+					"icon": "fa fa-angle-double-right",
+					"type": "view",
+					"collectionName": "tokhaiyte",
+					"route": "tokhaiyte/collection",
+					"$ref": "app/tokhaiyte/js/CollectionView",
+					"visible": function () {
+						return true
+					}
+				}
+
+			]
+		},
+		{
+			"text": "Báo cáo đơn vị",
+			"icon":static_url + "/images/icons/task_120.png",
+			"type":"category",
+			"entries":[
+				{
+					"text": "Tổng hợp nghi ngờ nhiễm bệnh",
+					"icon": "fa fa-calendar",
+					"type": "view",
+					"collectionName": "baocaotonghopnghingonhiembenhnhoma",
+					"route": "baocaotonghopnghingonhiembenhnhoma/collection",
+					"$ref": "app/view/BaoCaoTongHopNghiNgoNhiemBenh/ModelView",
+					"visible": function () {
+						return true
+					}
+				},
+				
+				{
+					"text": "Tờ khai y tế đối với người",
+					"icon": "fa fa-angle-double-right",
+					"type": "view",
+					"collectionName": "tokhaiyte",
+					"route": "tokhaiyte/collection",
+					"$ref": "app/tokhaiyte/js/CollectionView",
+					"visible": function () {
+						return true
+					}
+				}
+
+			]
 		},
 		
-		{
-			"text": "Tờ khai y tế đối với người",
-			"icon": "fa fa-angle-double-right",
-			"type": "view",
-			"collectionName": "baocaonghingonhiembenh",
-			"route": "baocaonghingonhiembenh/collection",
-			"$ref": "app/baocaonghingonhiembenh/js/BaoCaoNghiNgoNhiemBenhCollectionlView",
-			"visible": function () {
-				return true
-			}
-		},
-		{
-			"type": "view",
-			"collectionName": "baocaonghingonhiembenh",
-			"route": "baocaonghingonhiembenh/model",
-			"$ref": "app/baocaonghingonhiembenh/js/BaoCaoNghiNgoNhiemBenhModelView",
-			"visible": function () {
-				return false;
-			}
-		},
 	];
 });
