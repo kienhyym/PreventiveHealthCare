98f0be9c355989369b78145d9edb9e76128462d0
kiennd <kiennd@healthchain.vn>
2020-02-07 15:44:56 +0700

    nav

--- static/app/nav/nav.js
+++ static/app/nav/nav.js
@@ -4,28 +4,38 @@ define(function (require) {
 		_ = require('underscore'),
 		Gonrin = require('gonrin');
 	return [
-		{
-			"text": "Báo cáo tổng hợp nghi ngờ nhiễm bệnh",
-			"icon": "fa fa-calendar",
-			"type": "view",
-			"collectionName": "baocaotonghopnghingonhiembenhnhoma",
-			"route": "baocaotonghopnghingonhiembenhnhoma/collection",
-			"$ref": "app/baocaotonghopnghingonhiembenhnhoma/view/ModelView",
-			"visible": function () {
-				return true
-			}
-		},
+		// {
+		// 	"text": "Báo cáo tổng hợp nghi ngờ nhiễm bệnh",
+		// 	"icon": "fa fa-calendar",
+		// 	"type": "view",
+		// 	"collectionName": "baocaotonghopnghingonhiembenhnhoma",
+		// 	"route": "baocaotonghopnghingonhiembenhnhoma/collection",
+		// 	"$ref": "app/baocaotonghopnghingonhiembenhnhoma/view/ModelView",
+		// 	"visible": function () {
+		// 		return true
+		// 	}
+		// },
+		
 		{
 			"text": "Tờ khai y tế đối với người",
-			"icon": "fa fa-book",
+			"icon": "fa fa-angle-double-right",
 			"type": "view",
-			"collectionName": "chitietthietbi",
-			"route": "tokhaiyte/collection",
-			"$ref": "app/tokhaiyte/js/CollectionView",
+			"collectionName": "tokhaiytedoivoinguoi",
+			"route": "tokhaiytedoivoinguoi/collection",
+			"$ref": "app/tokhaiytedoivoinguoi/js/CollectionView",
 			"visible": function () {
 				return true
+
 			}
 		},
-		
+		{
+			"type": "view",
+			"collectionName": "tokhaiytedoivoinguoi",
+			"route": "tokhaiytedoivoinguoi/model",
+			"$ref": "app/tokhaiytedoivoinguoi/js/ModelView",
+			"visible": function () {
+				return false;
+			}
+		},
 	];
 });
