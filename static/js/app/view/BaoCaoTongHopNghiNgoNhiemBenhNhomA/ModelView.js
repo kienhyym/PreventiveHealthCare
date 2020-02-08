1779d9fc4369f390c706571210f43f0d2433a8c7
Bay cao Ước mơ <baycaouocmo8@gmail.com>
2020-02-08 12:56:20 +0700

    update

--- static/js/app/view/BaoCaoTongHopNghiNgoNhiemBenhNhomA/ModelView.js
+++ static/js/app/view/BaoCaoTongHopNghiNgoNhiemBenhNhomA/ModelView.js
@@ -29,7 +29,6 @@ define(function (require) {
 								label: "TRANSLATE:BACK",
 								command: function(){
 									var self = this;
-									//Backbone.history.history.back();
 					                self.getApp().getRouter().navigate("baocaotonghopnghingonhiembenhnhoma/collection");
 								}
 							},
@@ -40,20 +39,12 @@ define(function (require) {
 				    	    	label: "TRANSLATE:SAVE",
 				    	    	command: function(){
                                     var self = this;
-                                    // var ngaybaocao = self.$el.find("#ngaybaocao").val();
-				                    // ngaybaocao = parseInt(ngaybaocao) ? parseInt(ngaybaocao) : 0;
-                                    // self.model.set("ngaybaocao",ngaybaocao);
-                                   
-				    	    		// if(!self.validate()){
-				    	    		// 	return;
-				    	    		// }
 				                    self.model.save(null,{
 				                        success: function (model, respose, options) {
 				                            self.getApp().notify("Save successfully");
 				                            self.getApp().getRouter().navigate("baocaotonghopnghingonhiembenhnhoma/collection");
 				                        },
 				                        error: function (model, xhr, options) {
-				                            //self.alertMessage("Something went wrong while processing the model", false);
 				                            self.getApp().notify('Save error');
 				                        }
 				                    });
@@ -74,7 +65,6 @@ define(function (require) {
 								var id = this.model.get("id"); 
 								var url = "/export/excel/baocaotonghopnghingonhiembenhnhoma/" + id;
 								 window.open(url, "_blank");
-								// self.getApp().getRouter().navigate(self.collectionName + "/collection");
 							},
 							visible: function(){
 								var id =  this.getApp().getRouter().getParam("id");
@@ -107,11 +97,9 @@ define(function (require) {
             if (user) {
                 var id = this.getApp().getRouter().getParam("id");
                 if (id) {
-                    //progresbar quay quay
                     this.model.set('id', id);
                     this.model.fetch({
                         success: function (data) {
-                            // self.$el.find("#ngaybaocao").val(self.model.get("ngaybaocao"));
                             var info = user.info;
                             var cuakhau = info.cuakhau;
                             if ( !!cuakhau && cuakhau !== null && cuakhau !== "" && cuakhau !== undefined) {
@@ -120,16 +108,23 @@ define(function (require) {
                                 }
 							}
                             self.applyBindings();
-                            self.registerEvent();
+							self.registerEvent();
+							self.getDanhsachnhiembenh();
                         },
                         error: function () {
                             self.getApp().notify("Get data Eror");
                         },
                     });
                 } else {
-					self.model.set("ngaybaocao", moment().startOf('day').unix());
+					
+					// console.log
+					self.model.set("ngaybaocao",new Date());
                     self.applyBindings();
 					self.registerEvent();
+					self.model.on("change:ngaybaocao",function () {
+						var ngaybaocao = self.model.get("ngaybaocao");
+						self.getDanhsachnhiembenh(ngaybaocao);
+					})
 					var info = user.info;
                     var cuakhau = info.cuakhau;
                     if ( !!cuakhau && cuakhau !== null && cuakhau !== "" && cuakhau !== undefined) {
@@ -162,7 +157,7 @@ define(function (require) {
 				self.$el.find(".user_donvi").hide();
 				self.$el.find(".user_cuakhau").show();
 			} else if(self.checkRole("DonViUser") || self.checkRole("DonViAdmin")) {
-				console.log("234");
+				
 				self.model.set("loaibaocao",1);
 				self.$el.find(".user_donvi").show();
 				self.$el.find(".user_cuakhau").hide();
@@ -177,13 +172,6 @@ define(function (require) {
 					self.$el.find(".user_cuakhau").show();
 				}
 			});
-			var danhsach = new NguoiNghiNhiemView();
-			// var thongtincon = new NguoiNghiNhiemView({ "viewData": { "id": con.id, "con": con, "me": me, "bo": bo } });
-				self.$el.find("#add-nghingonhiembenh-item").empty();
-				danhsach.render();
-				self.$el.find("#content").append(danhsach.el);
-			
-
 		},
 		validate: function() {
 			var self = this;
@@ -194,7 +182,92 @@ define(function (require) {
 			}
 			return true;
 			
-		}
+		},
+		getDanhsachnhiembenh:function(ngaybaocao){
+			var self = this;
+			
+				$("#add-nghingonhiembenh-item").html("");
+				var url_danhsachnghingo = self.getApp().serviceURL + '/api/v1/baocaonghingonhiembenh';
+				$.ajax({
+				url: url_danhsachnghingo,
+					method: "GET",
+		    		data: {"q": JSON.stringify({"filters": {"ngaybaocao":{"$eq":ngaybaocao}},"page":1}),"results_per_page":50},
+					contentType: "application/json",
+					success: function (data) {
+						console.log(data);
+						$("#add-nghingonhiembenh-item").grid({
+		                	showSortingIndicator: true,
+		                	onValidateError: function(e){
+		                		console.log(e);
+		                	},
+		                	language:{
+		                		no_records_found:" "
+		                	},
+		                	noResultsClass:"alert alert-default no-records-found",
+		                	refresh:true,
+		                	orderByMode: "client",
+		                	fields: [
+								{
+									field: "id", label: "ID", width: 250, readonly: true,
+								},
+				
+								// { field: "ma", label: "Mã", width: 250 },
+								{field: "fullname", label: "Họ và tên", sortable: {order:"asc"},width: 250},
+								{field: "phone_national_number", label: "Số điện thoại", width: 250},
+								{ field: "ngaybaocao", label: "Ngày báo cáo", width: 250 },
+								{ field: "noibaocao", label: "Nơi báo cáo", width: 250 },
+								// { field: "nambaocao", label: "Năm báo cáo", width: 250 },
+								// { field: "donvi_id", label: "ID đơn vị", width: 250 },
+								{ field: "donvi", label: "Tên đơn vị", width: 250, textField: "ten" },
+								
+								// {field: "email", label: "Email"},
+								// {field: "id", label: "Mã cán bộ"},
+								// {
+								// 	field: "roles",
+								// 	label: "Vai trò",
+								// 	// textField: "name",
+								// 	template: function(rowData) {
+								// 		var roles = rowData.roles;
+								// 		if (roles.indexOf("admin_donvi")){
+								// 			return '<span>Admin</span>';
+								// 		} else {
+								// 			return '<span>Canbo</span>';
+								// 		}
+								// 	}
+								// },
+							],
+							dataSource: data.objects,
+							primaryField:"id",
+							selectionMode: "single",
+							pagination: {
+							page: 1,
+							pageSize: 20
+							},
+							onRowClick: function(event){
+								if (event.rowId) {
+									var path =  'baocaonghingonhiembenh/model/model?id=' + event.rowId;
+									gonrinApp().getRouter().navigate(path);
+								}
+		                    },
+		                });
+					},
+					error: function (xhr, status, error) {
+						try {
+							if (($.parseJSON(xhr.responseText).error_code) === "SESSION_EXPIRED"){
+								self.getApp().notify("Hết phiên làm việc, vui lòng đăng nhập lại!");
+								self.getApp().getRouter().navigate("login");
+							} else {
+								self.getApp().notify({ message: $.parseJSON(xhr.responseText).error_message }, { type: "danger", delay: 1000 });
+							}
+						}
+						catch (err) {
+						  self.getApp().notify({ message: "Lỗi truy cập dữ liệu, vui lòng thử lại sau"}, { type: "danger", delay: 1000 });
+						}
+					},
+				});
+			
+			
+		},
 	});
 
 });
