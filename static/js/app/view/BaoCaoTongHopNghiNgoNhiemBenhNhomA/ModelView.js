d702963246fe8c506bee3ee00de4980919c8e473
CuongNC <cuongnc@gonrin.com>
2020-02-10 04:38:31 +0700

    update view

--- static/js/app/view/BaoCaoTongHopNghiNgoNhiemBenhNhomA/ModelView.js
+++ static/js/app/view/BaoCaoTongHopNghiNgoNhiemBenhNhomA/ModelView.js
@@ -299,21 +299,21 @@ define(function (require) {
 									self.createNguoiNhiemBenh();
 								}
 							},
-							{
-								name: "export_excel",
-								type: "button",
-								buttonClass: "btn-primary btn-sm",
-								label: "Xuất Excel danh sách",
-								command: function() {
-									var ngaybaocao = self.model.get("ngaybaocao");
-									var url = "/export/excel/baocaotonghopnghingonhiembenhnhoma";
+							// {
+							// 	name: "export_excel",
+							// 	type: "button",
+							// 	buttonClass: "btn-primary btn-sm",
+							// 	label: "Xuất Excel danh sách",
+							// 	command: function() {
+							// 		var ngaybaocao = self.model.get("ngaybaocao");
+							// 		var url = "/export/excel/baocaotonghopnghingonhiembenhnhoma";
 									
-									if(!!ngaybaocao){
-										url = "/export/excel/baocaotonghopnghingonhiembenhnhoma?ngaybaocao=" + ngaybaocao;
-									}
-									window.open(url, "_blank");
-								}
-							},
+							// 		if(!!ngaybaocao){
+							// 			url = "/export/excel/baocaotonghopnghingonhiembenhnhoma?ngaybaocao=" + ngaybaocao;
+							// 		}
+							// 		window.open(url, "_blank");
+							// 	}
+							// },
 						],
 						fields: [
 							{
